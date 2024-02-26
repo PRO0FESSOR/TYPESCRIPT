0ea8c25812fb4f979c9baef697fa9cd4961099d0
PRO0FESSOR <siddharth96549@gmail.com>
2024-02-26 21:05:21 +0530

    typescript basics

---
+++ BASICS/SAMPLE.js
@@ -0,0 +1,6 @@
+var a = 454545;
+console.log(a);
+var pass = "myname";
+console.log(pass);
+var b = "hello sir";
+console.log(b);
---
+++ BASICS/SAMPLE.ts
@@ -0,0 +1,11 @@
+const a = 454545;
+console.log(a)
+
+const pass = "myname";
+console.log(pass);
+
+let b = "hello sir";
+console.log(b);
+
+console.log(a , pass , b)
+
