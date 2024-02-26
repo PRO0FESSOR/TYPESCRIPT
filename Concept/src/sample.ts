d8fea75af4a0f45646ec470a60fac48c4438a412
PRO0FESSOR <siddharth96549@gmail.com>
2024-02-26 22:34:03 +0530

    type declaration , union and type keyword , functions

--- Concept/src/sample.ts
+++ Concept/src/sample.ts
@@ -1,2 +1,41 @@
 let a = 35;
-console.log(a);
+// console.log(a);
+
+
+// type declaration
+
+let stringType :string;
+let inttype:number;
+let booltype:boolean;
+
+//different type declaration
+
+let anothertype = <number>67;
+
+
+// union type declaration
+
+let surname : string | number ;
+
+surname = 85;
+
+// function 
+
+
+const fun = (n:number,m:number) => {
+    return n+m;
+}
+
+//another type declaration  (type is used here )
+
+type username = number | string;
+
+let differnttype :username = 55;
+
+type funs = ( n:number , m:number) => number;
+
+let difffuntype:funs = (n,m) => n+m;
+
+
+
+
